20fbf08e7096287a480f8026d7f2367d15af5092
Jeffrey Meadows <jmeadows@box.com>
2014-12-09 18:42:14 -0800

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def main():
     base_dir = dirname(__file__)
     setup(
         name='flaky',
-        version='1.0.0',
+        version='1.0.1',
         description='Plugin for nose or py.test that automatically reruns flaky tests.',
         long_description=open(join(base_dir, 'README.rst')).read(),
         author='Box',
