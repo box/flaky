ce0dfaafd8bf0257b154b890f6583fc25cab5faf
Jeffrey Meadows <jmeadows@box.com>
2015-10-08 10:12:54 -0700

    Improve ensure_unicode_string.
    Fixes #59 relates to #46.

--- flaky/utils.py
+++ flaky/utils.py
@@ -9,8 +9,21 @@ except NameError:
     unicode_type = str
 
 
-def ensure_unicode_string(string):
+def ensure_unicode_string(obj):
+    """
+    Return a unicode string representation of the given obj.
+
+    :param obj:
+        The obj we want to represent in unicode
+    :type obj:
+        varies
+    :rtype:
+        `unicode`
+    """
     try:
-        return unicode_type(string)
+        return unicode_type(obj)
     except UnicodeDecodeError:
-        return string.decode('utf-8', 'replace')
+        if hasattr(obj, 'decode'):
+            return obj.decode('utf-8', 'replace')
+        else:
+            return str(obj).decode('utf-8', 'replace')
