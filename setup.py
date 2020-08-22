f7a213be93c5c5607b8258508853d1e0ef73b2dd
Hugo <hugovk@users.noreply.github.com>
2020-08-22 12:18:28 +0300

    Drop support for EOL Python 2.7

--- setup.py
+++ setup.py
@@ -13,13 +13,12 @@ CLASSIFIERS = [
     'License :: OSI Approved :: Apache Software License',
     'Topic :: Software Development :: Testing',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3 :: Only',
     'Programming Language :: Python :: Implementation :: CPython',
     'Programming Language :: Python :: Implementation :: PyPy',
     'Operating System :: OS Independent',
@@ -76,7 +75,7 @@ def main():
             ]
         },
         keywords='nose pytest plugin flaky tests rerun retry',
-        python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+        python_requires='>=3.5',
         classifiers=CLASSIFIERS,
     )
 
